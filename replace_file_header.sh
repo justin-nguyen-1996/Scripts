3ff4e8b64fdac2a2880128a7a1611d46bd8b73b4
Justin Nguyen <2014justinnguyen@gmail.com>
2017-06-09 13:34:02 -0700

    update

--- replace_file_header.sh
+++ replace_file_header.sh
@@ -1,5 +1,10 @@
+
 # rename (asdf.*) to (bb.*)
-# (adsf.c) & (asdf.qwer) to (bb.c) & (bb.qwer)
-for f in asdf.*; do 
-mv -- "$f" "bb.${f#asdf.}"
+# (adsf.c) & (asdf.qwer) --> (bb.c) & (bb.qwer)
+
+old_header="asdf"
+new_header="bb"
+
+for f in $old_header.*; do 
+mv -- $f $new_header.${f#$old_header.}
 done
